5418531b97bbdd84953a540bf734612c86a64a87
Roman Blanyar <romanb@oss-usa.com>
2017-07-02 01:14:31 +0300

    Added Projects page

--- server.js
+++ server.js
@@ -48,6 +48,10 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs');
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Errror'
